d472d5ff9b7ca853681f6394c8870c2a223026e3
hattinghj <info@johan.nl.eu.org>
2026-07-30 08:29:35 +0400

    Rename blom.png -> blossom.jpg (converted to JPG)

--- blossom.js
+++ blossom.js
@@ -36,7 +36,7 @@ requestAnimationFrame(fadeTrail);
 function createBlossom() {
   if (document.querySelectorAll(".blossom").length > 8) return;
   const blossom = document.createElement("img");
-  blossom.src = "blom.png";
+  blossom.src = "blossom.jpg";
   blossom.classList.add("blossom");
   blossom.style.left = Math.random() * 100 + "vw";
   blossom.style.animationDuration = (20 + Math.random() * 15) + "s";
